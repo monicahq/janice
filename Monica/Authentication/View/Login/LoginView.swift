7cf7467345a2c4b9d6d7c9128af281d4ab17db87
Julien Hamon <julien.hamon404@gmail.com>
2020-02-17 18:25:48 -0500

    fix Sonar
    fix sonarcloud

--- Monica/Authentication/View/Login/LoginView.swift
+++ Monica/Authentication/View/Login/LoginView.swift
@@ -16,76 +16,81 @@ struct LoginView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                VStack(spacing: 0) {
-                    VStack(alignment: .leading, spacing: 0) {
-                        VStack(alignment: .leading) {
-                            Text("Welcome back 👋")
-                                .font(.system(size: 20.0, weight: .semibold, design: .rounded))
-                                .padding(15)
-                            Divider()
-                                .frame(height: 1)
-                                .background(Color("GrayBackground"))
-                        }
-                        .background(Color.white)
-                        .padding(.bottom, 17)
-
-                        VStack {
-
-                            Divider()
-                                .frame(height: 1)
-                                .background(Color("GrayBackground"))
-                            LabelTextFieldView(value:self.$emailAddress
-                                ,label: "email"
-                                ,placeHolder: "placeholder_email",
-                                 isSecure: false)
-                                .padding(.horizontal, 15)
-                                .padding(.top, 15)
-                                .padding(.bottom, 5)
-
-                            Divider()
-                                .frame(height: 1)
-                                .background(Color("GrayBackground"))
-                                .padding(.horizontal, 15)
-
-                            LabelTextFieldView(value:self.$password
-                                ,label: "password"
-                                ,placeHolder: "placeholder_password",
-                                 isSecure: true)
-                                .padding(.horizontal, 15)
-                                .padding(.top, 15)
-                                .padding(.bottom, 5)
-
-                            Divider()
-                                .frame(height: 1)
-                                .background(Color("GrayBackground"))
-                        }
-                        .background(Color.white)
-                    }
-
-
-                    Button(action: {
-                        self.viewModel.apply(.onButtonCick(login: self.$emailAddress,Password: self.$password))
-                    }) {
-                        Text("signin")
-                            .foregroundColor(Color.white)
-                            .font(.system(size: 17.0, weight: .semibold, design: .rounded))
-                    }
-                    .alert(item: self.$viewModel.haveError, content: { text -> Alert in
-                                    Alert(title: Text("Important message"), message: Text("alert_login_error"), dismissButton: .default(Text("ok")))
-
-                    })
-                    .frame(width: geometry.size.width - 30, height: 50.0)
-                    .background(Color("MonicaBlue"))
-                    .cornerRadius(8)
-                    .padding(.top, 30)
-
-                    Spacer()
-
-                }
+                self.loginScreen(geometry: geometry)
             }
             .edgesIgnoringSafeArea(.bottom)
             .background(Color("GrayBackground"))
         }
+    }
+
+    private func loginScreen(geometry: GeometryProxy) -> some View {
+        VStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+                VStack(alignment: .leading) {
+                    Text("Welcome back 👋")
+                        .font(.system(size: 20.0, weight: .semibold, design: .rounded))
+                        .padding(15)
+                    Divider()
+                        .frame(height: 1)
+                        .background(Color("GrayBackground"))
+                }
+                .background(Color.white)
+                .padding(.bottom, 17)
+
+                VStack {
+
+                    Divider()
+                        .frame(height: 1)
+                        .background(Color("GrayBackground"))
+                    LabelTextFieldView(value:self.$emailAddress
+                        ,label: "email"
+                        ,placeHolder: "placeholder_email",
+                         isSecure: false)
+                        .padding(.horizontal, 15)
+                        .padding(.top, 15)
+                        .padding(.bottom, 5)
+
+                    Divider()
+                        .frame(height: 1)
+                        .background(Color("GrayBackground"))
+                        .padding(.horizontal, 15)
+
+                    LabelTextFieldView(value:self.$password
+                        ,label: "password"
+                        ,placeHolder: "placeholder_password",
+                         isSecure: true)
+                        .padding(.horizontal, 15)
+                        .padding(.top, 15)
+                        .padding(.bottom, 5)
+
+                    Divider()
+                        .frame(height: 1)
+                        .background(Color("GrayBackground"))
+                }
+                .background(Color.white)
+            }
+
+
+            Button(action: {
+                self.viewModel.apply(.onButtonCick(login: self.$emailAddress,Password: self.$password))
+            }) {
+                Text("signin")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 17.0, weight: .semibold, design: .rounded))
+            }
+            .alert(item: self.$viewModel.haveError, content: { text -> Alert in
+                            Alert(title: Text("Important message"), message: Text("alert_login_error"), dismissButton: .default(Text("ok")))
+
+            })
+            .frame(width: geometry.size.width - 30, height: 50.0)
+            .background(Color("MonicaBlue"))
+            .cornerRadius(8)
+            .padding(.top, 30)
+
+            Spacer()
+
+        }
+
     }
 }
 
